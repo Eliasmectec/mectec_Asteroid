c8aa8628bf591358a97f827390a8981316cc749b
mectec <eliasmectec@gmail.com>
2025-10-15 18:29:16 -0400

    Primeiro commit - Projeto Asteroid

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+
+def main():
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+def fill (self,color):
+    display.flip(black)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from game-project-mectec!")
+
+
+if __name__ == "__main__":
+    main()
