2f77dad8a4e189db37cd9a7714c02dfee192494b
mectec <eliasmectec@gmail.com>
2025-10-15 19:05:18 -0400

    fiz some erro sintex

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-def fill (self,color):
-    display.flip(black)
+        screen.fill('black')
+
+        pygame.display.flip()
 
 
 
